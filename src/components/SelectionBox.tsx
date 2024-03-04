0da6e781427d8099974e82a03ffa5203c6df8d2a
Liz Robson <lizziearobson@gmail.com>
2024-03-04 15:06:45 +0000

    added checkbox and usestate

--- src/components/SelectionBox.tsx
+++ src/components/SelectionBox.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ImCheckboxChecked, ImCheckboxUnchecked } from "react-icons/im";
-
 
 type Props = {}
 
 export default function SelectionBox({}: Props) {
+  
+  const [isChecked, setIsChecked] = useState(false);
+   
+  const handleClick = () => {
+    setIsChecked(!isChecked);
+  }
+
+
   return (
     <>
-    <ImCheckboxChecked />
-    <ImCheckboxUnchecked />
+      <div className="bg-blue-200 rounded-lg p-4 w-1/3 h-96">
+        {isChecked ? <ImCheckboxChecked onClick={handleClick} /> : <ImCheckboxUnchecked onClick={handleClick} />}
+      </div>
     </>
-  )
+  );
+  
 }
