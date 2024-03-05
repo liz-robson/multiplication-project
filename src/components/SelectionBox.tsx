3b64a14badf63b953567b6f894ccded9140814be
Liz Robson <lizziearobson@gmail.com>
2024-03-05 20:34:31 +0000

    checkbox functionality working correctly

--- src/components/SelectionBox.tsx
+++ src/components/SelectionBox.tsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import { ImCheckboxChecked, ImCheckboxUnchecked } from "react-icons/im";
 
 export default function SelectionBox(props: Props) {
   
-  const [isChecked, setIsChecked] = useState(false);
-
-  const [isSelected, setIsSelected] = useState(false);
-   
   const handleClick = () => {
-    setIsChecked(!isChecked);
+    props.onSelect(props.table.id)
+    // setIsChecked(!isChecked);
   }
 
   return (
@@ -18,7 +15,10 @@ export default function SelectionBox(props: Props) {
           <h1  className="mb-4 text-5xl">{props.table.tableSize}</h1>
           <p className="mb-16 text-2xl">Complete {props.table.sumNumber} sums</p>
         </div>
-        {isChecked ? <ImCheckboxChecked  className="w-8 h-8" onClick={handleClick} /> : <ImCheckboxUnchecked  className="w-8 h-8" onClick={handleClick} />}
+        {props.selectedId === props.table.id ? 
+        <ImCheckboxChecked  className="w-8 h-8" onClick={handleClick} /> : 
+        <ImCheckboxUnchecked  className="w-8 h-8" onClick={handleClick} />
+        }
         <p className="mb-4 text-2xl absolute bottom-0 right-5">5 mins</p>
         </div>
     </>
--- src/components/SelectionList.tsx
+++ src/components/SelectionList.tsx
@@ -1,11 +1,12 @@
 import SelectionBox from "./SelectionBox";
+import { useState } from 'react';
 
 export default function SelectionList(props) {
 
-  const [selected, setSelected] = useState(null);
+  const [selectedId, setSelected] = useState(null);
 
   const handleSelect = (id) => {
-    setSelected(id === selected ? null : id);
+    setSelected(id === selectedId ? null : id);
   }
 
     const objects = props.tableList;
@@ -13,7 +14,7 @@ export default function SelectionList(props) {
   return (
     <div className="grid grid-cols-3 gap-6">
     {objects.map((table, index) => {
-        return <SelectionBox table={table} key={index} onSelect={handleSelect} selected={selected}/>;
+        return <SelectionBox table={table} key={index} onSelect={handleSelect} selectedId={selectedId}/>;
     })}
     </div>
   );
